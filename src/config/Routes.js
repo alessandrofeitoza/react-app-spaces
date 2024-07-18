b47858ce499d8c21476a6a8b18ef5f05b2a46eb4
Sara Camilo <saraccamilo@gmail.com>
2024-07-18 20:15:25 -0300

    create reservation

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,12 +1,14 @@
 import { Route, Routes as ReactRoutes } from "react-router-dom";
 import Rooms from "../pages/Rooms";
 import Home from "../pages/Home";
+import Reservation from "../pages/Reservation";
 
 export default function Routes(){
     return (
         <ReactRoutes>
             <Route path="/" element={<Home/>}/>
             <Route path="/salas" element={<Rooms/>}/>
+            <Route path="/reservas" element={<Reservation/>}/>
             <Route path="/*" element={<div>Pagina nao encontrada</div>}/>
         </ReactRoutes>
     );
